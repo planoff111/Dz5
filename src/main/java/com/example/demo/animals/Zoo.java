580b9ee639aa7cddea8fec7b488ac56baeb57a71
planoff111 <planoff111@gmail.com>
2023-06-07 18:24:07 +0300

    Revert "Add a + operation in dev"
    This reverts commit 79c5908c9d60fcf8695a85c7ac13f995af06406e.

--- src/main/java/com/example/demo/animals/Zoo.java
+++ src/main/java/com/example/demo/animals/Zoo.java
@@ -5,9 +5,6 @@ public class Zoo {
 
     public static void main(String[] args) {
         System.out.println("Hello Zoo!");
-        int num1 = 1;
-        int num2 = 2;
-        System.out.println(num2 + num1);
 
     }
 
