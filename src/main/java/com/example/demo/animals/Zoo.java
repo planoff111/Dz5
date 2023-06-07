c28516bcbd22c52a7decc33d22dea805e6681efa
planoff111 <planoff111@gmail.com>
2023-06-07 18:45:55 +0300

    Add a int variable
    (cherry picked from commit 7412336ce1ca0cd678902a334cf5658cbd4dbd40)

--- src/main/java/com/example/demo/animals/Zoo.java
+++ src/main/java/com/example/demo/animals/Zoo.java
@@ -5,6 +5,8 @@ public class Zoo {
 
     public static void main(String[] args) {
         System.out.println("Hello Zoo!");
+        int i = 1;
+
         System.out.println("hello wirlD");
     }
 
