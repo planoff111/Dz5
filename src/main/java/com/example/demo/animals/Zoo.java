56509b88a87aa4a3e47ebc5ede6f74663c6a2d37
planoff111 <planoff111@gmail.com>
2023-06-07 20:01:36 +0300

    Revert "amend commit"
    This reverts commit 0cf2e677f1b1aafb1468914a3a351c8f1d1f57f3.

--- src/main/java/com/example/demo/animals/Zoo.java
+++ src/main/java/com/example/demo/animals/Zoo.java
@@ -6,8 +6,6 @@ public class Zoo {
     public static void main(String[] args) {
         System.out.println("Hello Zoo!");
         System.out.println("hello wirlD");
-        System.out.println("Hello Zoo!");
-
     }
 
 
