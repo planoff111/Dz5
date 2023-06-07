b7487322ad74dad131dea6728a081370fcbef613
planoff111 <planoff111@gmail.com>
2023-06-07 19:59:48 +0300

    amend commit

--- src/main/java/com/example/demo/animals/Zoo.java
+++ src/main/java/com/example/demo/animals/Zoo.java
@@ -6,6 +6,15 @@ public class Zoo {
     public static void main(String[] args) {
         System.out.println("Hello Zoo!");
         System.out.println("hello wirlD");
+
+
+
+
+        System.out.println("hello wirlD");
+
+
+
+        System.out.println("hello wirlD");
     }
 
 
